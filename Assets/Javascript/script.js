27bcb230da9cee57f22aafd7b2cc24aaf8406e1c
Leozinho2831 <leoferraz1818@gmail.com>
2024-09-05 22:19:39 -0300

    commit: function darkMode and darkMode saved in storage

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,1 +1,32 @@
 import getWord from './words.js';
+
+const inputCheckbox = document.querySelector('#checkbox');
+const classBody = 'dark';
+
+function darkModeActive(){
+    const darkModeActive = localStorage.getItem('darkMode');
+
+    if(darkModeActive === 'true'){
+
+        inputCheckbox.checked = true;
+        document.body.classList.add(classBody);
+
+    } else {
+
+        inputCheckbox.checked = false;
+        document.body.classList.remove(classBody);
+
+    }
+}
+
+darkModeActive();
+
+if(inputCheckbox){
+    function darkMode(){
+        document.body.classList.toggle(classBody);
+        
+        localStorage.setItem('darkMode', inputCheckbox.checked);
+    }
+    
+    inputCheckbox.onchange = darkMode;
+}
